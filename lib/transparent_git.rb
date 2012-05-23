7e299139bfbe1c80aea1baa4336d5d8f277aaac9
Mike Harris <mharris@indians.com>
2012-05-23 12:06:40 -0400

    different input methods for remote_track

--- lib/transparent_git.rb
+++ lib/transparent_git.rb
@@ -13,6 +13,15 @@ ensure
   Dir.chdir(old)
 end
 
+class Object
+  def blank?
+    to_s.strip == ''
+  end
+  def present?
+    !blank?
+  end
+end
+
 class RemoteTracker
   include FromHash
   attr_accessor :repo_holding_dir, :name, :working_dir
@@ -25,12 +34,13 @@ class RemoteTracker
     ENV['GIT_WORK_TREE'] = working_dir
   end
   def commit_current_state!
+    puts "doing #{working_dir}"
     setup_env!
     FileUtils.mkdir_p(repo_holding_dir)
 
     lock = "#{repo_dir}/refs/heads/master.lock"
     FileUtils.rm(lock) if FileTest.exist?(lock)
-    
+
     ec "#{git} init"
     ec "#{git} add -u"
     ec "#{git} add ."
@@ -63,13 +73,26 @@ class RemoteTrackers
   class << self
     def create_from_yaml(f)
       str = File.read(f)
+      puts str
+      raise "file is empty" if str.blank?
+
       h = YAML::load(str)
+      puts h.inspect
+
       res = new
-      repos = h.delete('repos')
-      raise "no repos given" unless repos
-      res.from_hash(h)
-      repos.each do |repo|
-        res.add(repo)
+
+      if h.kind_of?(Hash)
+        repos = h.delete('repos')
+        raise "no repos given" unless repos
+        res.from_hash(h)
+        repos.each do |repo|
+          res.add(repo)
+        end
+      else
+        repos = str.split("\n").map { |x| x.strip }.select { |x| x.present? }
+        repos.each do |r|
+          res.add :working_dir => r
+        end
       end
       res
     end
